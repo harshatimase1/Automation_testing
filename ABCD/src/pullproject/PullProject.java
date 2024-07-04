057198ddc7437ea827275948a4d0bc74ea3dae27
harshatimase1 <harshatimase93@gmail.com>
2024-07-04 13:15:40 +0530

    Added pullproject

---
+++ ABCD/src/pullproject/PullProject.java
@@ -0,0 +1,13 @@
+package pullproject;
+
+public class PullProject {
+   public void name() {
+	System.out.println("PullProject");
+	   
+}
+	public static void main(String[] args) {
+     PullProject p=new PullProject();
+     p.name();
+	}
+
+}
