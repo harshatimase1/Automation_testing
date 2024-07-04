950782a0022d612626894ae318a6ea83a734e26d
harshatimase1 <harshatimase93@gmail.com>
2024-07-04 10:21:15 +0530

    PullProject

---
+++ ABC/src/pullProject/Pullpractice.java
@@ -0,0 +1,16 @@
+package pullProject;
+
+public class Pullpractice {
+	
+	public void name() {
+		System.out.println("Pull the project");
+		
+	}
+
+	public static void main(String[] args) {
+
+		Pullpractice p=new Pullpractice();
+		p.name();
+	}
+
+}
