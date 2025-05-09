59608d221dcde3a4b04279c4c692cc4ede0bf4a1
harshatimase1 <harshatimase93@gmail.com>
2025-05-09 17:33:14 +0530

    Pull Practice project

---
+++ practice_pullProject/src/practice_pullProject1/PracticePullProject.java
@@ -0,0 +1,12 @@
+package practice_pullProject1;
+
+public class PracticePullProject {
+	int a = 5;
+
+	public static void main(String[] args) {
+		PracticePullProject p = new PracticePullProject();
+		System.out.println(p.a);
+
+	}
+
+}
